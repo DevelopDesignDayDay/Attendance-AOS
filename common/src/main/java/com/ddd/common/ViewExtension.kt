edaec5a690df9d116224b5a8423e78f918f69619
Gyongun Min <mindev93@foodfly.co.kr>
2019-09-20 16:56:05 +0900

    add : extension about view

--- common/src/main/java/com/ddd/common/ViewExtension.kt
+++ common/src/main/java/com/ddd/common/ViewExtension.kt
@@ -2,20 +2,15 @@ package com.ddd.common
 
 import android.app.Activity
 import android.content.Context
-import android.graphics.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.view.animation.AlphaAnimation
 import android.widget.Toast
 import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
-import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.BitmapImageViewTarget
 
 
 fun ViewGroup.recyclerViewInflate(@LayoutRes layout: Int, attach: Boolean = false): View {
@@ -38,73 +33,16 @@ fun View.gone() {
     this.visibility = View.GONE
 }
 
+fun View.aniVisible() {
+    val animation = AlphaAnimation(0f, 1f).apply { duration = 200 }
+    this.visible()
+    this.animation = animation
+}
+
+fun View.aniGone() {
+    val animation = AlphaAnimation(1f, 0f).apply { duration = 200 }
+    this.gone()
+    this.animation = animation
+}
+
 fun Context.toast(mgs: CharSequence) = Toast.makeText(this, mgs, Toast.LENGTH_SHORT).show()
-
-
-    fun <T> ImageView.loadCircularImage(
-    model: T,
-    borderSize: Float = 0F,
-    borderColor: Int = Color.WHITE
-) {
-    Glide.with(context)
-        .asBitmap()
-        .load(model)
-        .apply(RequestOptions.circleCropTransform())
-        .into(object : BitmapImageViewTarget(this) {
-            override fun setResource(resource: Bitmap?) {
-                setImageDrawable(
-                    resource?.run {
-                        RoundedBitmapDrawableFactory.create(
-                            resources,
-                            if (borderSize > 0) {
-                                createBitmapWithBorder(borderSize, borderColor)
-                            } else {
-                                this
-                            }
-                        ).apply {
-                            isCircular = true
-                        }
-                    }
-                )
-            }
-        })
-}
-
-fun Bitmap.createBitmapWithBorder(borderSize: Float, borderColor: Int = Color.WHITE): Bitmap {
-    val borderOffset = (borderSize * 2).toInt()
-    val halfWidth = width / 2
-    val halfHeight = height / 2
-    val circleRadius = Math.min(halfWidth, halfHeight).toFloat()
-    val newBitmap = Bitmap.createBitmap(
-        width + borderOffset,
-        height + borderOffset,
-        Bitmap.Config.ARGB_8888
-    )
-
-    // Center coordinates of the image
-    val centerX = halfWidth + borderSize
-    val centerY = halfHeight + borderSize
-
-    val paint = Paint()
-    val canvas = Canvas(newBitmap).apply {
-        // Set transparent initial area
-        drawARGB(0, 0, 0, 0)
-    }
-
-    // Draw the transparent initial area
-    paint.isAntiAlias = true
-    paint.style = Paint.Style.FILL
-    canvas.drawCircle(centerX, centerY, circleRadius, paint)
-
-    // Draw the image
-    paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
-    canvas.drawBitmap(this, borderSize, borderSize, paint)
-
-    // Draw the createBitmapWithBorder
-    paint.xfermode = null
-    paint.style = Paint.Style.STROKE
-    paint.color = borderColor
-    paint.strokeWidth = borderSize
-    canvas.drawCircle(centerX, centerY, circleRadius, paint)
-    return newBitmap
-}
