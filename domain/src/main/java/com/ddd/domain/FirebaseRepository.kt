ffc34d715ae2e9b556b5eb401b670bb27f6ed201
mkw8263 <ruddns8213@naver.com>
2019-09-22 23:21:58 +0900

    add : domain layer, check manager usecase

--- domain/src/main/java/com/ddd/domain/FirebaseRepository.kt
+++ domain/src/main/java/com/ddd/domain/FirebaseRepository.kt
@@ -1,5 +1,8 @@
 package com.ddd.domain
 
+import com.ddd.common.DDDException
+
 interface FirebaseRepository {
-    fun saveUser(email: String, name: String, type: String,isManager:Boolean)
+    fun saveUser(email: String, name: String, position: String, isManager: Boolean)
+    fun isManager(uuid: String, result: (Boolean) -> Unit, error: (DDDException) -> Unit)
 }
