426629779216cc187c115499ae8114d0fc54c895
mkw8263 <ruddns8213@naver.com>
2019-09-22 23:17:05 +0900

    create : common module, exception & message

--- common/src/main/java/com/ddd/common/DDDException.kt
+++ common/src/main/java/com/ddd/common/DDDException.kt
@@ -1,5 +1,9 @@
 package com.ddd.common
 
 sealed class DDDException : Exception() {
+    //Login
     object ExpireSession : DDDException()
+
+    //FireBase
+    data class NotFindDataBaseData(val msg: String) : DDDException()
 }
--- common/src/main/java/com/ddd/common/MessageManager.kt
+++ common/src/main/java/com/ddd/common/MessageManager.kt
@@ -6,4 +6,7 @@ object MessageManager {
     const val LOGIN_VALIDATE_NOT_PASSWORD = "유효한 비밀번호가 아닙니다."
     const val LOGIN_INPUT_ERROR_MSG = "이메일 혹은 비밀번호를 확인해주세요."
     const val LOGIN_SIGN_UP_FAIL_ERROR_MSG = "회원가입에 실패하였습니다."
+
+    //error
+    const val ERROR_NOT_FOUND_FIREBASE_DATABASE_DATA = "요청 데이터가 존재하지 않습니다."
 }
